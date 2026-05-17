8d763320eb52b961813836fb8c20fbd984b13220
User <user@macbookpro.home>
2026-05-17 23:29:48 +0100

    userDetails

---
+++ Auth/src/main/java/dev/joaogj/Auth/controller/TesteController.java
@@ -0,0 +1,15 @@
+package dev.joaogj.Auth.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TesteController {
+
+    @GetMapping
+    public String test(){
+        return "testendo segurança";
+    }
+}
---
+++ Auth/src/main/java/dev/joaogj/Auth/entity/User.java
@@ -0,0 +1,56 @@
+package dev.joaogj.Auth.entity;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "users")
+@Setter
+@Getter
+
+public class User  implements UserDetails{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+    
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+   
+}
