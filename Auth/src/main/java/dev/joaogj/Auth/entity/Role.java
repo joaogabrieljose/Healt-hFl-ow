826b8e1b50652e91f8a01474d13d84c303719bc6
User <user@macbookpro.home>
2026-05-18 16:33:11 +0100

    Role

---
+++ Auth/src/main/java/dev/joaogj/Auth/entity/Role.java
@@ -0,0 +1,7 @@
+package dev.joaogj.Auth.entity;
+
+public enum Role {
+    // perfil de permição de usuario
+    ROLE_USER, 
+    ROLE_ADMIN
+}
--- Auth/src/main/java/dev/joaogj/Auth/entity/User.java
+++ Auth/src/main/java/dev/joaogj/Auth/entity/User.java
@@ -1,15 +1,25 @@
 package dev.joaogj.Auth.entity;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
@@ -27,9 +37,16 @@ public class User  implements UserDetails{
     private String email;
     private String password;
 
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles", joinColumns = @JoinColumn(name = "userId"))
+    @Enumerated(EnumType.STRING)
+    @Column(name = "role")
+    private Set<Role> roles = new HashSet<>();
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of();
+        return roles.stream().map(r-> new SimpleGrantedAuthority(r.name()))
+        .toList();
     }
 
     @Override
