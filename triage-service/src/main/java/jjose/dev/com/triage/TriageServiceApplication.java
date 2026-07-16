898c26bc99e2d940c76a41bcf73de8bb2250b0d2
User <user@macbookpro.home>
2026-07-16 11:19:52 +0100

    config openFeign

--- triage-service/src/main/java/jjose/dev/com/triage/TriageServiceApplication.java
+++ triage-service/src/main/java/jjose/dev/com/triage/TriageServiceApplication.java
@@ -2,8 +2,10 @@ package jjose.dev.com.triage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class TriageServiceApplication {
 
     public static void main(String[] args) {
